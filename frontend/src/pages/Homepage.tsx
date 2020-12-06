a2282dca7c43993cd4ecdbb2d81def724be62834
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-12-05 22:46:12 -0300

    settingProducts

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,10 +4,12 @@ import {View} from 'react-native';
 //import {Map} from  
 //import Map from '../components
 
-const Maptruck = () => {
+const Homepage = () => {
   return (
     <View>
       <Map />
     </View>
   )
 }
+
+export default Homepage;
